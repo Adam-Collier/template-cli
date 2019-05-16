1e96d5060c23ec45852c1dee0ef8f247ee6fc5f8
Adam Collier <adamcollierart@gmail.com>
2019-05-16 16:06:48 +0100

    now uses async await, improves readability

--- template-create.js
+++ template-create.js
@@ -4,28 +4,29 @@ const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 let path = require("path");
 
-let createTemplate = (workingDir, name) => {
-  let newDir = `${workingDir}/${name}`;
-  fs.copy(path.join(__dirname, "template"), newDir)
-    .then(() => {
-      fetch("https://www.missguided.co.uk/")
-        .then(response => response.text())
-        .then(data => {
-          const $ = cheerio.load(data);
-          $(".main").html('<div data-include="index"></div>');
-          $("body").append('<script src="main.js"></script>');
-          data = $.html();
-          fs.writeFile(`${newDir}/index.html`, data, err => {
-            if (err) console.log(err);
-            console.log(
-              chalk.green.bold("Your template has been created successfully \n")
-            );
-          });
-        })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.error(err));
-};
+async function createTemplate(workingDir, name) {
+  try {
+    let newDir = `${workingDir}/${name}`;
+    await fs.copy(path.join(__dirname, "template"), newDir);
+
+    let response = await fetch("https://www.missguided.co.uk/help");
+    let data = await response.text();
+
+    const $ = cheerio.load(data);
+
+    $(".main").html('<div data-include="index"></div>');
+    $("body").append('<script src="main.js"></script>');
+
+    data = $.html();
+
+    await fs.writeFile(`${newDir}/index.html`, data);
+    console.log(
+      chalk.green.bold("Your template has been created successfully \n")
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 module.exports = {
   createTemplate
--- template-update.js
+++ template-update.js
@@ -1,47 +1,51 @@
 const fs = require("fs-extra");
 const chalk = require("chalk");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const { promisify } = require("util");
 
-let updateTemplate = workingDir => {
-  fs.readdir(workingDir, function(err, items) {
+const writeFile = promisify(fs.writeFile);
+
+async function updateTemplate(workingDir) {
+  await fs.readdir(workingDir, async function(err, items) {
     if (err) console.log(err);
     let success = ["views", "package.json", "main.js", "index.html"].every(
       val => items.includes(val)
     );
     if (success) {
-      fetch("https://www.missguided.co.uk/")
-        .then(response => response.text())
-        .then(data => {
-          const $ = cheerio.load(data);
-          $(".main").html('<div data-include="index"></div>');
-          $("body").append('<script src="main.js"></script>');
-          data = $.html();
-          fs.writeFile(`${workingDir}/index.html`, data, err => {
-            if (err) console.log(err);
-            console.log(
-              chalk.gray.bgGreen.bold(
-                "Your template has been successfully updated"
-              )
-            );
-          });
-        })
-        .catch(err => console.log(err));
+      let response = await fetch("https://www.missguided.co.uk/help");
+      let data = await response.text();
 
-      fs.readdir(`${workingDir}/views`, function(err, items) {
-        if (items.includes("your-html.html")) {
-          fs.rename(
-            `${workingDir}/views/your-html.html`,
-            `${workingDir}/views/index.html`
-          );
-          console.log(chalk.gray.bgGreen.bold("your views were updated"));
-        } else {
-          console.log(chalk.white.bgRed.bold("the file doesn't exist"));
-        }
-      });
+      const $ = cheerio.load(data);
+
+      $(".main").html('<div data-include="index"></div>');
+      $("body").append('<script src="main.js"></script>');
+
+      data = $.html();
+
+      await fs.writeFile(`index.html`, data);
+      console.log(
+        chalk.gray.bgGreen.bold(
+          "Your template has been created successfully \n"
+        )
+      );
+
+      let directories = await fs.readdir(`${workingDir}/views`);
+
+      if (directories.includes("your-html.html")) {
+        fs.rename(
+          `${workingDir}/views/your-html.html`,
+          `${workingDir}/views/index.html`
+        );
+        console.log(chalk.gray.bgGreen.bold("your views were updated"));
+      } else {
+        console.log(chalk.white.bgRed.bold("view already correctly named"));
+      }
     } else {
       console.log(chalk.white.bgRed.bold("this isn't a template directory"));
     }
   });
-};
+}
 
 module.exports = {
   updateTemplate
